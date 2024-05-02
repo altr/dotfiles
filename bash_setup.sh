4b8d9b8bad3c56e3aa77d4c8ec136d679b0b7b24
Al <alain@altr.ch>
2024-05-02 01:25:09 +0000

    Update bash_setup.sh

--- bash_setup.sh
+++ bash_setup.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
-# install nerd font system wide
-
-zip_file="DejaVuSansMono.zip"
-font_dir="/usr/local/share/fonts"; mkdir -p "$font_dir"
-
-if ls $font_dir/DejaVuSans* &> /dev/null; then
-	echo "DejaVuFont already exists"
-else
-	curl -fLo DejaVuSansMono.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/$zip_file"
-	sudo unzip -q "$zip_file" -d "$font_dir" || { echo "Error: Unable to extract '$selected_font'."; return 1; }
-	rm "$zip_file"
-	if command -v fc-cache &> /dev/null; then
-		fc-cache -f > /dev/null || { echo "Error: Unable to update font cache. Exiting."; exit 1; }
-		echo "Font cache updated."
-	else
-		sudo apt-get update
-		sudo apt install fontconfig
-		fc-cache -f > /dev/null || { echo "Error: Unable to update font cache. Exiting."; exit 1; }
-	fi
-fi
-
+# setup bash
 if ! command -v starship &> /dev/null;then
 	echo "install starship"
 	cmd=$(curl -sS https://starship.rs/install.sh | sh -s -- -f)
 fi
 
-if ! command -v lt &> /dev/null;then
+# ensure the right aliases are added to bashrc
+if ! command -v grep 'eval "$(starship init bash)"' ~/.bashrc &> /dev/null;then
 	echo "setup .bashrc"
 cat <<EOF>> ~/.bashrc 
 alias ll='ls -alF'
@@ -40,10 +21,15 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 
+# GIT
+alias gp='git pull'
+alias gs='git status'
 # ~~~~~~~~~~~~~~~ Prompt ~~~~~~~~~~~~~~~~~~~~~~~~
 EOF
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
-fi
 
-source ~/.bashrc
+echo "please run"
+echo "source ~/.bashrc"
+
+fi
